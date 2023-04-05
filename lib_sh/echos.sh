e597c538fc4630a62a9f0b7e3273dae2b5a8796d
ekweible <ekweible@gmail.com>
2023-04-05 08:09:41 -0600

    Format echos.sh

--- lib_sh/echos.sh
+++ lib_sh/echos.sh
@@ -22,37 +22,37 @@ REVERSE=$(tput smso)
 UNDERLINE=$(tput smul)
 
 function br() {
-  printf "\n"
+    printf "\n"
 }
 
 function info() {
-  printf "%s\n" "$CYAN[info]$NORMAL $1"
+    printf "%s\n" "$CYAN[info]$NORMAL $1"
 }
 
 function ok() {
-  printf "%s\n" "$GREEN[ok]$NORMAL $1"
+    printf "%s\n" "$GREEN[ok]$NORMAL $1"
 }
 
 function bot() {
-  printf "\n%s\n" "$GREEN\[._.]/$NORMAL - $1"
+    printf "\n%s\n" "$GREEN\[._.]/$NORMAL - $1"
 }
 
 function group() {
-  printf "\n%s\n\n" "$YELLOW[-----]$NORMAL $1"
+    printf "\n%s\n\n" "$YELLOW[-----]$NORMAL $1"
 }
 
 function prompt() {
-  read -p $'\x1b[32;01m?\x1b[39;49;00m'" $1 " "${@:2}"
+    read -p $'\x1b[32;01m?\x1b[39;49;00m'" $1 " "${@:2}"
 }
 
 function running() {
-  printf "%s\n" "$YELLOW ⇒ $NORMAL$1"
+    printf "%s\n" "$YELLOW ⇒ $NORMAL$1"
 }
 
 function warn() {
-  printf "%s\n" "$YELLOW[warning]$NORMAL $1"
+    printf "%s\n" "$YELLOW[warning]$NORMAL $1"
 }
 
 function error() {
-  printf "%s\n" "$RED[error]$NORMAL $1"
+    printf "%s\n" "$RED[error]$NORMAL $1"
 }
