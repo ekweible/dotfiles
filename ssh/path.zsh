7999d1cd9177a46a2b8311587627da5798a28b0d
ekweible <ekweible@gmail.com>
2023-04-08 08:44:46 -0600

    ssh-agent shell config update

--- ssh/path.zsh
+++ ssh/path.zsh
@@ -1,1 +1,6 @@
-eval "$(ssh-agent -s)" > /dev/null
+# Start ssh-agent and load SSH key
+if [ -z "$SSH_AUTH_SOCK" ]; then
+    eval "$(ssh-agent -s)"
+fi
+
+ssh-add --apple-use-keychain ~/.ssh/*.id_ed25519
