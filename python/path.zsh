fee84d1b118875eda9934156313cc20e9ba68ef2
evanweible-wf <evan.weible@workiva.com>
2025-08-13 10:22:20 -0600

    pyenv updates

--- python/path.zsh
+++ python/path.zsh
@@ -1,13 +1,4 @@
-pyenv() {
-  eval "$(command pyenv init -)"
-  eval "$(command pyenv virtualenv-init -)"
-  export PATH=$(pyenv root)/shims:$PATH
-  pyenv "$@"
-}
-
-# # Initialize pyenv and its virtualenv plugin.
-# if command -v pyenv &> /dev/null; then
-#     eval "$(pyenv init -)"
-#     eval "$(pyenv virtualenv-init -)"
-#     export PATH=$(pyenv root)/shims:$PATH
-# fi
+export PYENV_ROOT="$HOME/.pyenv"
+[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init - zsh)"
+eval "$(command pyenv virtualenv-init -)"
