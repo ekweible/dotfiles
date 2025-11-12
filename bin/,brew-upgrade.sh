bd12061b494faf694bb5cd3413ecc50032be58b0
ekweible <evan@weible.me>
2025-11-12 11:56:13 -0700

    Remove unnecessary bins

--- bin/,brew-upgrade.sh
+++ bin/,brew-upgrade.sh
@@ -7,15 +7,9 @@ cd "$(dirname "${BASH_SOURCE}")/.."
 source ./lib_sh/echos.sh
 source ./lib_sh/linkers.sh
 
-# Run this first to ensure brew and all declared deps are installed
-./bin/,brew-bundle.sh
-
 running "brew upgrade"
 brew upgrade
 running "brew upgrade --cask --greedy"
 brew upgrade --cask --greedy
-
-# Run this again to update the Brewfile.lock.json
-./bin/,brew-bundle.sh
 
 ok
