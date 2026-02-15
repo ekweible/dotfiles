63ba97a982c00170b1424c68e13cf21819f4c0ce
ekweible <evan@weible.me>
2026-02-15 10:36:18 -0700

    Use zed as default editor

--- home/dot_config/zsh/env.zsh
+++ home/dot_config/zsh/env.zsh
@@ -2,7 +2,9 @@
 export DOTFILES="$HOME/.config/dotfiles"
 
 # Preferred editor hierarchy
-if command -v cursor &> /dev/null; then
+if command -v zed &> /dev/null; then
+    export EDITOR="zed"
+elif command -v cursor &> /dev/null; then
     export EDITOR="cursor"
 elif command -v code &> /dev/null; then
     export EDITOR="code"
