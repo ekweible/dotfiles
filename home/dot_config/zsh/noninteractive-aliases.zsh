541cf003a6e4a1ea1d8694e7eaa977e80a53c9b0
ekweible <evan@weible.me>
2026-04-09 11:43:35 -0600

    Restore Ghostty prompt integration after reload

--- home/dot_config/zsh/noninteractive-aliases.zsh
+++ home/dot_config/zsh/noninteractive-aliases.zsh
@@ -10,7 +10,6 @@ alias ls='lsd'
 
 # === Utility aliases ===
 alias path='echo -e ${PATH//:/\\n}'
-alias reload="exec $SHELL -l"
 
 # Add more commonly-used aliases here that you want available
 # in non-interactive contexts (like chat command prompts)
